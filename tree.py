b98e8f99d28e0e8ee8b2da70d0ed09ed3fb9fce5
Devendra saini <tonysaini1998@gmail.com>
2019-06-21 23:37:48 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[123]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+
+# In[11]:
+
+
+#thuis is for apple and orange
+features=[ [100,0], [120,0],[130,1],[150,1]   ]
+#here 0 means for smooth and 1 for bumpy
+
+
+# In[3]:
+
+
+label=['apple','apple','orange','orange']
+
+
+# In[6]:
+
+
+#callling Decisiontreeclassifier
+clf=DecisionTreeClassifier()
+
+
+# In[12]:
+
+
+#now time for training data
+trained=clf.fit(features,label)
+
+
+# In[128]:
+
+
+#now predicting
+trained.predict([[10,1]])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
